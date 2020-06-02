5616affa16c160c1716f73dd4042f5a95f30fde7
mannv <mannv7032@co-well.com.vn>
2020-06-02 15:17:47 +0700

    update mysql init

--- config/mysql/initdb.d/init.sql
+++ config/mysql/initdb.d/init.sql
@@ -1,14 +1,8 @@
--- SET GLOBAL time_zone = '+07:00';
--- SET time_zone = '+07:00';
--- SELECT @@GLOBAL.time_zone, @@SESSION.time_zone;
+SET GLOBAL time_zone = '+07:00';
+SET time_zone = '+07:00';
+SELECT @@GLOBAL.time_zone, @@SESSION.time_zone;
 
 CREATE DATABASE IF NOT EXISTS `db_web` COLLATE 'utf8mb4_unicode_ci';
 GRANT ALL ON `db_web`.* TO 'default'@'%';
-
-CREATE DATABASE IF NOT EXISTS `db_craw` COLLATE 'utf8mb4_unicode_ci';
-GRANT ALL ON `db_craw`.* TO 'default'@'%';
-
-CREATE DATABASE IF NOT EXISTS `db_news` COLLATE 'utf8mb4_unicode_ci';
-GRANT ALL ON `db_news`.* TO 'default'@'%';
 
 FLUSH PRIVILEGES;
