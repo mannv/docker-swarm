460ed5422480aa9c56ca82491575723ce54fbdd8
mannv <mannv7032@co-well.com.vn>
2020-06-02 15:24:49 +0700

    commit code

--- config/mysql/initdb.d/init.sql
+++ config/mysql/initdb.d/init.sql
@@ -5,4 +5,6 @@ SELECT @@GLOBAL.time_zone, @@SESSION.time_zone;
 CREATE DATABASE IF NOT EXISTS `db_web` COLLATE 'utf8mb4_unicode_ci';
 GRANT ALL ON `db_web`.* TO 'default'@'%';
 
+ALTER USER 'default' IDENTIFIED WITH mysql_native_password BY 'secret';
+
 FLUSH PRIVILEGES;
